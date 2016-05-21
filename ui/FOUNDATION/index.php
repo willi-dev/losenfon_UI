e1ae495cac791adefbb81bade30ee412e45fe29e
willi-dev <willi.ilmukomputer@gmail.com>
2016-05-21 13:43:13 +0700

    add base file

---
+++ ui/FOUNDATION/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+	<title>Lost n Found: Here, you can publish your lost & found!</title>
+
+    <link rel="stylesheet" href="../assets/css/foundation.css" />
+    <script src="../assets/js/vendor/modernizr.js"></script>
+
+</head>
+<body>
+	
+	<div id="lf-page" class="">
+			
+		<div class="row">
+			<div class="large-12 medium-12 columns">
+				
+				<div class="large-6 medium-12 large-centered medium-centered columns">
+					<!-- <div class="row collapse">
+						<div class="large-9 columns">
+							<input type="text" name="" id="">
+						</div>
+						<div class="large-3 columns">
+							<span class="postfix">
+								<input type="submit" value="" class="medium alert button">
+							</span>
+						</div>
+					</div> -->
+					<input type="text" name="lf-input-search" id="lf-input-search" placeholder="Cari di sini!" >
+				</div>
+
+			</div> 
+		</div>
+
+	</div>
+
+	<script src="../assets/js/vendor/jquery.js"></script>
+    <script src="../assets/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+    
+</body>
+</html>
---
+++ ui/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+	<title>Lost n Found: Here, you can publish your lost & found!</title>
+
+    <link rel="stylesheet" href="../assets_foundation/css/foundation.css" />
+    <script src="../assets_foundation/js/vendor/modernizr.js"></script>
+
+</head>
+<body>
+	
+	<div id="lf-page" class="">	
+		<div id="lf-page-header">
+			<div class="row">
+				<div class="large-12 medium-12 columns">
+					
+					<div class="large-6 medium-12 large-centered medium-centered columns">
+						<!-- <div class="row collapse">
+							<div class="large-9 columns">
+								<input type="text" name="" id="">
+							</div>
+							<div class="large-3 columns">
+								<span class="postfix">
+									<input type="submit" value="" class="medium alert button">
+								</span>
+							</div>
+						</div> -->
+						<input type="text" name="lf-input-search" id="lf-input-search" placeholder="Cari di sini!" >
+					</div>
+
+				</div> 
+			</div>
+		</div>
+		<div id="lf-page-content">
+			
+		</div>
+		<div id="lf-page-footer">
+			
+		</div>		
+		
+
+	</div>
+
+	<script src="../assets_foundation/js/vendor/jquery.js"></script>
+    <script src="../assets_foundation/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+    
+</body>
+</html>
