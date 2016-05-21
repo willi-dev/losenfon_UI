42f1d4d1f6dea22c6f80ee59ecf018f74ad362aa
willi-dev <willi.ilmukomputer@gmail.com>
2016-05-21 14:00:49 +0700

    change index file base with bootstrap style base

--- ui/index.php
+++ ui/index.php
@@ -1,54 +1,97 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<html>
     
-	<title>Lost n Found: Here, you can publish your lost & found!</title>
-
-    <link rel="stylesheet" href="../assets_foundation/css/foundation.css" />
-    <script src="../assets_foundation/js/vendor/modernizr.js"></script>
-
-</head>
-<body>
-	
-	<div id="lf-page" class="">	
-		<div id="lf-page-header">
-			<div class="row">
-				<div class="large-12 medium-12 columns">
-					
-					<div class="large-6 medium-12 large-centered medium-centered columns">
-						<!-- <div class="row collapse">
-							<div class="large-9 columns">
-								<input type="text" name="" id="">
-							</div>
-							<div class="large-3 columns">
-								<span class="postfix">
-									<input type="submit" value="" class="medium alert button">
-								</span>
-							</div>
-						</div> -->
-						<input type="text" name="lf-input-search" id="lf-input-search" placeholder="Cari di sini!" >
-					</div>
-
-				</div> 
-			</div>
-		</div>
-		<div id="lf-page-content">
-			
-		</div>
-		<div id="lf-page-footer">
-			
-		</div>		
-		
-
-	</div>
-
-	<script src="../assets_foundation/js/vendor/jquery.js"></script>
-    <script src="../assets_foundation/js/foundation.min.js"></script>
-    <script>
-      $(document).foundation();
-    </script>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+        rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="style.css">
+    </head>
     
-</body>
+    <body>
+        <div class="cover">
+            <div class="navbar">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"><span>Lost &amp; Found</span></a>
+                    </div>
+                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active">
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Contacts</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="cover-image" style="background-image: url(https://unsplash.imgix.net/photo-1418065460487-3e41a6c84dc5?q=25&amp;fm=jpg&amp;s=127f3a3ccf4356b7f79594e05f6c840e);"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h1 class="text-inverse text-left">
+                            <div class="form-group col-md-6">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Keyword">
+                                    <span class="input-group-btn">
+                                        <a class="btn btn-danger" type="submit">Go</a>
+                                    </span>
+                                </div>
+                            </div>
+                        </h1>
+                        <br>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <a href="#"><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a>
+                                    <h3 class="text-primary">Heading</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <a href="#"><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a>
+                                    <h3 class="text-primary">Heading</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <a href="#"><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a>
+                                    <h3 class="text-primary">Heading</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <a href="#"><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a>
+                                    <h3 class="text-primary">Heading</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <a href="#"><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a>
+                                    <h3 class="text-primary">Heading</h3>
+                                </div>
+                                <div class="col-md-4">
+                                    <a href="#"><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a>
+                                    <h3 class="text-primary">Heading</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
 </html>
